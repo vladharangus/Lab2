0f886d835d6aded15e2f4f648233e0a7dc40e2ca
vladharangus <vlad.hara99@gmail.com>
2020-10-27 21:30:07 +0200

    ScannerUpdate2

--- src/entities/scanner/Scanner.java
+++ src/entities/scanner/Scanner.java
@@ -18,7 +18,7 @@ public class Scanner {
     private final SymbolTable symbolTable;
     private final ArrayList<Pair<String, Integer>> pif = new ArrayList<Pair<String, Integer>>();
     private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/" , "<" , ">" , "=", "==", "<=", ">="));
-    private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("\\[", "\\]", "\\}", "\\{", " ", ";", ")", "("));
+    private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("[", "]", "\\}", "\\{", " ", ";", ")", "("));
 
     public ArrayList<Pair<String, Integer>> getPif() {
         return pif;
@@ -35,20 +35,25 @@ public class Scanner {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(program));
             String line = bufferedReader.readLine();
+            int indexLine = 0;
             while (line != null) {
-
+                indexLine++;
                 String copyLine = line;// we make a copy of the current line
+                //line = line.replaceAll("\\s+","");
                 //we split the line by separators in order to get identifiers, constants, reserved words or operators
-                String[] tokens = line.split("[\\(||//) ;\\{||\\}]");
-                for(String token: tokens)
+                String[] tokens = line.split("[\\[||\\(||\\)||\\;||\\s||\\{||\\}||\\]]+");
+                for(int i = 0; i < tokens.length; i++)
                 {
                     //System.out.println(token);
+                    String token = tokens[i];
+                    if (token.equals(""))
+                        continue;
                     if (operators.contains(token) || reservedWords.contains(token))
                     {
                         pif.add(new Pair<>(token, -1));
                     }
                     else {
-                        if (token.matches("^[a-z]([a-zA-Z][0-9])*") || token.matches("[+-]?[1-9][0-9]*") || token.matches("0") ||
+                        if (token.matches("^[a-z][a-zA-Z0-9]*") || token.matches("[+-]?[1-9][0-9]*") || token.matches("0") ||
                                 token.matches("\"[0-9A-Za-z_]+\"")) {
                             Node n = symbolTable.search(symbolTable.root,token);
                             if(n != null)
@@ -62,10 +67,36 @@ public class Scanner {
                                 pif.add(new Pair<>(token, pos));
                             }
                         }
+                        else if(token.contains("\""))
+                        {
+                            StringBuilder stringBuilder = new StringBuilder(token + " ");
+                            int index = i + 1;
+                            String nextToken = tokens[index];
+                            while(!nextToken.contains("\"")) {
+                                stringBuilder.append(nextToken);
+                                stringBuilder.append(" ");
+                                index++;
+                                nextToken = tokens[index];
+                            }
+                            stringBuilder.append(tokens[index]);
+                            i = index;
+                            Node n = symbolTable.search(symbolTable.root,stringBuilder.toString());
+                            if(n != null)
+                            {
+                                int pos = n.position;
+                                pif.add(new Pair<>(stringBuilder.toString(), pos));
+                            }
+                            else {
+                                symbolTable.insert(stringBuilder.toString());
+                                int pos = symbolTable.search(symbolTable.root,stringBuilder.toString()).position;
+                                pif.add(new Pair<>(stringBuilder.toString(), pos));
+                            }
+                        }
                         else
                         {
+                            System.out.println(line);
                             System.out.println(token);
-                            System.out.println("Lexical error");
+                            System.out.println("Line " + indexLine + ": Lexical error");
                         }
                     }
                 }
