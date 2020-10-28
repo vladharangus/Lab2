0a4c07264b7873589e634fbc19b3030d3f547fdf
vladharangus <vlad.hara99@gmail.com>
2020-10-28 11:53:12 +0200

    ScannerUpdate

--- src/entities/scanner/Scanner.java
+++ src/entities/scanner/Scanner.java
@@ -17,8 +17,13 @@ public class Scanner {
     private final String program;
     private final SymbolTable symbolTable;
     private final ArrayList<Pair<String, Integer>> pif = new ArrayList<Pair<String, Integer>>();
-    private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/" , "<" , ">" , "=", "==", "<=", ">="));
-    private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("[", "]", "\\}", "\\{", " ", ";", ")", "("));
+
+    public SymbolTable getSymbolTable() {
+        return symbolTable;
+    }
+
+    private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/" , "<" , ">" , "=", "==", "<=", ">=", "%"));
+    private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("[", "]", "}", "{", ";", " ", ")", "("));
 
     public ArrayList<Pair<String, Integer>> getPif() {
         return pif;
@@ -31,6 +36,20 @@ public class Scanner {
         this.symbolTable = symbolTable;
     }
 
+    public void genPIF(String token, String Code) {
+        Node n = symbolTable.search(symbolTable.root,token);
+        if(n != null)
+        {
+            int pos = n.position;
+            pif.add(new Pair<>(Code, pos));
+        }
+        else {
+            symbolTable.insert(token);
+            int pos = symbolTable.search(symbolTable.root,token).position;
+            pif.add(new Pair<>(Code, pos));
+        }
+    }
+
     public void scanProgram() {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(program));
@@ -41,73 +60,54 @@ public class Scanner {
                 String copyLine = line;// we make a copy of the current line
                 //line = line.replaceAll("\\s+","");
                 //we split the line by separators in order to get identifiers, constants, reserved words or operators
-                String[] tokens = line.split("[\\[||\\(||\\)||\\;||\\s||\\{||\\}||\\]]+");
+                String[] tokens = line.split(" ");
                 for(int i = 0; i < tokens.length; i++)
                 {
                     //System.out.println(token);
                     String token = tokens[i];
-                    if (token.equals(""))
-                        continue;
-                    if (operators.contains(token) || reservedWords.contains(token))
+                    if (operators.contains(token) || reservedWords.contains(token) || separators.contains(token))
                     {
-                        pif.add(new Pair<>(token, -1));
+                        if (!token.equals(" "))
+                            pif.add(new Pair<>(token, -1));
                     }
                     else {
                         if (token.matches("^[a-z][a-zA-Z0-9]*") || token.matches("[+-]?[1-9][0-9]*") || token.matches("0") ||
                                 token.matches("\"[0-9A-Za-z_]+\"")) {
-                            Node n = symbolTable.search(symbolTable.root,token);
-                            if(n != null)
-                            {
-                                int pos = n.position;
-                                pif.add(new Pair<>(token, pos));
-                            }
-                            else {
-                                symbolTable.insert(token);
-                                int pos = symbolTable.search(symbolTable.root,token).position;
-                                pif.add(new Pair<>(token, pos));
-                            }
+
+                            if (token.matches("^[a-z][a-zA-Z0-9]*"))
+                                genPIF(token,"Identifier");
+                            if(token.matches("[+-]?[1-9][0-9]*") || token.matches("0"))
+                                genPIF(token, "Number_const");
+                            if (token.matches("\"[0-9A-Za-z_]+\""))
+                                genPIF(token, "String_const");
                         }
                         else if(token.contains("\""))
                         {
                             StringBuilder stringBuilder = new StringBuilder(token + " ");
                             int index = i + 1;
-                            String nextToken = tokens[index];
-                            while(!nextToken.contains("\"")) {
-                                stringBuilder.append(nextToken);
-                                stringBuilder.append(" ");
-                                index++;
-                                nextToken = tokens[index];
-                            }
-                            stringBuilder.append(tokens[index]);
-                            i = index;
-                            Node n = symbolTable.search(symbolTable.root,stringBuilder.toString());
-                            if(n != null)
-                            {
-                                int pos = n.position;
-                                pif.add(new Pair<>(stringBuilder.toString(), pos));
-                            }
+                            if(index == tokens.length)
+                                System.out.println("Line " + indexLine + ": Lexical error for token " + token);
                             else {
-                                symbolTable.insert(stringBuilder.toString());
-                                int pos = symbolTable.search(symbolTable.root,stringBuilder.toString()).position;
-                                pif.add(new Pair<>(stringBuilder.toString(), pos));
+
+                                String nextToken = tokens[index];
+                                while (index < tokens.length && !nextToken.contains("\"")) {
+                                    stringBuilder.append(nextToken);
+                                    stringBuilder.append(" ");
+                                    index++;
+                                    nextToken = tokens[index];
+                                }
+                                stringBuilder.append(tokens[index]);
+                                i = index;
+                                if (index == tokens.length)
+                                    System.out.println("Line " + indexLine + ": Lexical error for token " + token);
+                                else genPIF(stringBuilder.toString(),"String_const");
                             }
                         }
                         else
                         {
-                            System.out.println(line);
-                            System.out.println(token);
-                            System.out.println("Line " + indexLine + ": Lexical error");
+                            System.out.println("Line " + indexLine + ": Lexical error for token " + token);
                         }
                     }
-                }
-                //we parse again the line looking only for separators
-                int index = 0;
-                while (index < copyLine.length())
-                {
-                    String token = String.valueOf(copyLine.charAt(index));
-                    if (separators.contains(token))
-                        pif.add(new Pair<>(token, -1));
-                    index ++;
                 }
                 line = bufferedReader.readLine();
             }
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,10 +10,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         SymbolTable symbolTable = new SymbolTable();
-        Scanner scanner = new Scanner("p1.txt",symbolTable);
+        Scanner scanner = new Scanner("p1err.txt",symbolTable);
         scanner.scanProgram();
         ArrayList<Pair<String,Integer>> pif = scanner.getPif();
+        System.out.println("PIF:");
         for (Pair p: pif)
-            System.out.println(p.getL() + " " + p.getR());
+            System.out.println("Token: " + p.getL() + " Position: " + p.getR());
+        SymbolTable finalST = scanner.getSymbolTable();
+        System.out.println("Symbol Table:");
+        finalST.inorder();
     }
 }
