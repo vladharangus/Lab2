aa0f742f2b55566823f665b6df404d2f8917fc21
vladharangus <vlad.hara99@gmail.com>
2020-11-04 09:14:46 +0200

    ScannerBeta

--- src/entities/scanner/Scanner.java
+++ src/entities/scanner/Scanner.java
@@ -16,18 +16,20 @@ import java.util.regex.Pattern;
 public class Scanner {
     private final String program;
     private final SymbolTable symbolTable;
-    private final ArrayList<Pair<String, Integer>> pif = new ArrayList<Pair<String, Integer>>();
+    private final Pair[] pif = new Pair[300];
 
     public SymbolTable getSymbolTable() {
         return symbolTable;
     }
 
+    public Pair[] getPif() {
+        return pif;
+    }
+
     private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/" , "<" , ">" , "=", "==", "<=", ">=", "%"));
     private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("[", "]", "}", "{", ";", " ", ")", "("));
 
-    public ArrayList<Pair<String, Integer>> getPif() {
-        return pif;
-    }
+
 
     private final ArrayList<String> reservedWords = new ArrayList<>(Arrays.asList("if", "else", "then", "for", "execute", "const", "array", "read", "print", "number", "string"));
 
@@ -36,17 +38,17 @@ public class Scanner {
         this.symbolTable = symbolTable;
     }
 
-    public void genPIF(String token, String Code) {
+    public void genPIF(String token, String Code, int posPIF) {
         Node n = symbolTable.search(symbolTable.root,token);
         if(n != null)
         {
             int pos = n.position;
-            pif.add(new Pair<>(Code, pos));
+            pif[posPIF] = new Pair<>(Code, pos);
         }
         else {
             symbolTable.insert(token);
             int pos = symbolTable.search(symbolTable.root,token).position;
-            pif.add(new Pair<>(Code, pos));
+            pif[posPIF] = new Pair<>(Code, pos);
         }
     }
 
@@ -55,31 +57,33 @@ public class Scanner {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(program));
             String line = bufferedReader.readLine();
             int indexLine = 0;
+            int posPIF = 0;
             while (line != null) {
                 indexLine++;
                 String copyLine = line;// we make a copy of the current line
                 //line = line.replaceAll("\\s+","");
                 //we split the line by separators in order to get identifiers, constants, reserved words or operators
-                String[] tokens = line.split(" ");
+                String[] tokens = line.split("[\\[||\\(||\\)||\\;||\\s||\\{||\\}||\\]]+");
                 for(int i = 0; i < tokens.length; i++)
                 {
                     //System.out.println(token);
                     String token = tokens[i];
-                    if (operators.contains(token) || reservedWords.contains(token) || separators.contains(token))
+                    if (operators.contains(token) || reservedWords.contains(token))
                     {
-                        if (!token.equals(" "))
-                            pif.add(new Pair<>(token, -1));
+                        pif[posPIF] = new Pair<>(token, -1);
+                        posPIF++;
                     }
                     else {
                         if (token.matches("^[a-z][a-zA-Z0-9]*") || token.matches("[+-]?[1-9][0-9]*") || token.matches("0") ||
                                 token.matches("\"[0-9A-Za-z_]+\"")) {
 
                             if (token.matches("^[a-z][a-zA-Z0-9]*"))
-                                genPIF(token,"Identifier");
+                                genPIF(token,"Identifier",posPIF);
                             if(token.matches("[+-]?[1-9][0-9]*") || token.matches("0"))
-                                genPIF(token, "Number_const");
+                                genPIF(token, "Number_const",posPIF);
                             if (token.matches("\"[0-9A-Za-z_]+\""))
-                                genPIF(token, "String_const");
+                                genPIF(token, "String_const", posPIF);
+                            posPIF++;
                         }
                         else if(token.contains("\""))
                         {
@@ -100,7 +104,8 @@ public class Scanner {
                                 i = index;
                                 if (index == tokens.length)
                                     System.out.println("Line " + indexLine + ": Lexical error for token " + token);
-                                else genPIF(stringBuilder.toString(),"String_const");
+                                else genPIF(stringBuilder.toString(),"String_const", posPIF);
+                                posPIF++;
                             }
                         }
                         else
@@ -109,6 +114,14 @@ public class Scanner {
                         }
                     }
                 }
+               /* int index = 0;
+                while (index < copyLine.length())
+                {
+                    String token = String.valueOf(copyLine.charAt(index));
+                    if (separators.contains(token) && !token.equals(" "))
+                        pifadd(new Pair<>(token, -1));
+                    index ++;
+                }*/
                 line = bufferedReader.readLine();
             }
 
